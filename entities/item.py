ce697b84b7329c0d5ec066506407606b9395d584
hamoncito <popiel.agn@gmail.com>
2025-05-25 11:27:45 +0200

    change get_creator to abstract method, add comments to methods

--- entities/item.py
+++ entities/item.py
@@ -4,18 +4,27 @@ class Item(ABC):
     """
     Klasa abstrakcyjna, po której dziedziczą klasy Book, Magazine, DVD
     """
-    def __init__(self, id: str, title: str, year: int, creator: str):
+    def __init__(self, id: str, title: str, year: int):
         self.id = id
         self.title =  title
         self.year = year
-        self.creator = creator
         self.available = True # domyślnie pozycja jest dostępna
 
+    @abstractmethod
     def get_creator(self) -> str:
-        return self.creator
+        """
+        Metoda abstrakcyjna, zwracająca twórcę pozycji (autora, reżysera, redaktora)
+        """
+        pass
 
     def mark_unavailable(self) -> None:
+        """
+        Metoda oznaczająca pozycję jako wypożyczoną
+        """
         self.available = False
 
     def mark_available(self) -> None:
+        """
+        Metoda oznaczająca pozycję jako dostępną
+        """
         self.available = True
