f498cb2632f45b7a2938c0d0b1bdc3ee02a6a923
hamoncito <popiel.agn@gmail.com>
2025-05-25 11:35:24 +0200

    change get_creator implementation

--- entities/book.py
+++ entities/book.py
@@ -2,8 +2,7 @@ from entities.item import Item
 
 class Book(Item):
     def __init__(self, id: str, title: str, year: int, author: str):
-        super().__init__(id, title, year)
-        self.author = author
+        super().__init__(id, title, year, author)
 
     def get_creator(self) -> str:
-        return self.author
+        return self.creator
