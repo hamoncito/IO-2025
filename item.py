3afc45c1422647f19cb980a64b5578a15d5a4779
hamoncito <popiel.agn@gmail.com>
2025-05-25 11:10:17 +0200

    add item class

---
+++ item.py
@@ -0,0 +1,19 @@
+class Item:
+    """
+    Klasa abstrakcyjna, po której dziedziczą klasy Book, Magazine, DVD
+    """
+    def __init__(self, id: str, title: str, year: int, creator: str):
+        self.id = id
+        self.title =  title
+        self.year = year
+        self.creator = creator
+        self.available = True # domyślnie pozycja jest dostępna
+
+    def get_creator(self) -> str:
+        return self.creator
+
+    def mark_unavailable(self) -> None:
+        self.available = False
+
+    def mark_available(self) -> None:
+        self.available = True
