8c7206ec18d7d7180740aa7fbfe0bdefd8fbba7e
hamoncito <popiel.agn@gmail.com>
2025-06-04 19:19:37 +0200

    komentarze

--- entities/item.py
+++ entities/item.py
@@ -1,30 +1,37 @@
 from abc import ABC, abstractmethod
 
+
 class Item(ABC):
     """
-    Klasa abstrakcyjna, po której dziedziczą klasy Book, Magazine, DVD
+    Klasa abstrakcyjna reprezentująca pozycję w bibliotece, z której dziedziczą konkretne typy pozycji (Book, Magazine, DVD).
+
+    Atrybuty:
+        id (str): Identyfikator pozycji.
+        title (str): Tytuł pozycji.
+        year (int): Rok wydania pozycji.
+        available (bool) = True: Dostępność pozycji
+
+    Metody:
+        get_creator() -> str:
+            Metoda abstrakcyjna, zwracająca twórcę pozycji (autora, reżysera, redaktora).
+        mark_unavailable() -> None:
+            Oznacza pozycję jako wypożyczoną (niedostępną).
+        mark_available() -> None:
+            Oznacza pozycję jako dostępną.
     """
+
     def __init__(self, id: str, title: str, year: int):
         self.id = id
-        self.title =  title
+        self.title = title
         self.year = year
-        self.available = True # domyślnie pozycja jest dostępna
+        self.available = True
 
     @abstractmethod
     def get_creator(self) -> str:
-        """
-        Metoda abstrakcyjna, zwracająca twórcę pozycji (autora, reżysera, redaktora)
-        """
         pass
 
     def mark_unavailable(self) -> None:
-        """
-        Metoda oznaczająca pozycję jako wypożyczoną
-        """
         self.available = False
 
     def mark_available(self) -> None:
-        """
-        Metoda oznaczająca pozycję jako dostępną
-        """
         self.available = True
