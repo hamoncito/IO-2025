519356cab074d26da90d8f55cbd9920b7f7545b3
ompanek <ompanek@gmail.com>
2025-06-07 10:12:06 +0200

    Dodanie komentarzy do klasy dvd

--- entities/dvd.py
+++ entities/dvd.py
@@ -1,12 +1,30 @@
 from entities.item import Item
 
-class Dvd(Item):
-    def __init__(self, id: str, title: str, year: int, director: str, genre: str):
-        super().__init__(id, title, year, director)
-        self.genre = genre
+"""
+   Klasa dziedzicząca po klasie Item (pozycja) reprezentująca płytę DVD.
 
-    def get_creator(self) -> str:
-        return self.creator
+   Atrybuty:
+       id (str): Identyfikator płyty DVD.
+       title (str): Rok produkcji filmu.
+       year (int): Rok wydania płyty DVD.
+       director (str): Reżyser filmu.
+       genre (Genre): Gatunek filmu.
 
-    def get_genre(self) -> str:
-        return self.genre
+   Metody:
+       get_creator() -> str:
+           Zwraca reżysera filmu.
+        
+       def get_genre(self) -> str:
+            Zwraca gatunek filmu.
+   """
+
+from enum import Enum
+class Genre(Enum): ACTION = "Action" COMEDY = "Comedy" THRILLER= "Thriller" HORROR = "Horror" SCIENCE_FICTION= "Science Fiction" DOCUMENTARY = "Documentary" ROMANCE = "Romance"
+
+class Dvd(Item): def init(self, id: str, title: str, year: int, director: str, genre: Genre): super().init(id, title, year, director) self.genre = genre
+
+def get_creator(self) -> str:
+    return self.creator
+
+def get_genre(self) -> str:
+    return self.genre
