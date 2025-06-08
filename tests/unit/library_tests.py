a00949ccd62531a6a4ccc315dded2b9c8062f1cb
Wiktoria <wiktoria.pawlak@onet.com.pl>
2025-06-08 10:11:12 +0200

    Unit test - klasa member i library nowe zmiany

--- tests/unit/library_tests.py
+++ tests/unit/library_tests.py
@@ -1,66 +1,63 @@
 import unittest
-from entities.library import Library
+from datetime import datetime, timedelta
 from entities.item import Item
 from entities.member import Member
 from entities.rental import Rental
+from entities.library import Library
+
 
 class TestLibrary(unittest.TestCase):
     def setUp(self):
         self.book1 = Item(id="1", title="Wiedźmin", year=1990, creator="Sapkowski")
         self.book2 = Item(id="2", title="Dziady", year=1832, creator="Mickiewicz")
-        self.member1 = Member(
-            member_id="1",
-            name="Adam",
-            last_name="Mickiewicz",
-            email="adam@example.com",
-            rented_items=[]
-        )
+        self.book3 = Item(id="3", title="Narrenturm", year=2000, creator="Sapkowski")
 
-        self.member2 = Member(
-            member_id="2",
-            name="Juliusz",
-            last_name="Słowacki",
-            email="juliusz@example.com",
-            rented_items=[]
+        self.member1 = Member("1", "Adam", "Mickiewicz", "adam@pw.edu.pl", [])
+        self.member2 = Member("2", "Juliusz", "Słowacki", "julek@pw.edu.pl", [])
+
+        self.rental1 = Rental(
+            item=self.book1,
+            member=self.member1,
+            rent_date=datetime.now() - timedelta(days=20),
+            return_date=datetime.now() - timedelta(days=5),
         )
-        self.rental1 = Rental(is_overdue=False)
-        self.rental2 = Rental(is_overdue=True)
+        self.book1.mark_unavailable()
+        self.member1.rented_items.append(self.rental1)
+
+        self.rental2 = Rental(
+            item=self.book3,
+            member=self.member2,
+            rent_date=datetime.now() - timedelta(days=5),
+            return_date=datetime.now() + timedelta(days=10),
+        )
+        self.book3.mark_unavailable()
+        self.member2.rented_items.append(self.rental2)
 
         self.library = Library(
-            items=[self.book1, self.book2],
-            members=[self.member1],
+            items=[self.book1, self.book2, self.book3],
+            members=[self.member1, self.member2],
             rentals=[self.rental1, self.rental2],
-            daily_fine_for_overdue_items=2.0
+            daily_fine_for_overdue_items=2.0,
         )
 
-    def test_add_member(self):
-        self.library.add_member(self.member2)
-        self.assertIn(self.member2, self.library.get_all_members())
+    def test_get_most_popular_creator(self):
+        self.assertEqual(self.library.get_most_popular_creator(), "Sapkowski")
 
-    def test_add_item(self):
-        new_book = Item("Lalka")
-        self.library.add_item(new_book)
-        self.assertIn(new_book, self.library.get_all_items())
-
-    def test_get_all_items(self):
-        items = self.library.get_all_items()
-        self.assertEqual(len(items), 2)
-
-    def test_get_all_available_items(self):
-        available = self.library.get_all_avalible_items()  # literówka w nazwie!
-        self.assertEqual(len(available), 1)
-        self.assertEqual(available[0].title, "Wiedźmin")
-
-    def test_get_all_rentals(self):
-        rentals = self.library.get_all_rentals()
-        self.assertEqual(len(rentals), 2)
+    def test_get_all_member_rented_from_library(self):
+        rentals = self.library.get_all_member_rented_from_library(self.member1)
+        self.assertEqual(rentals, [self.rental1])
 
     def test_get_overdue_rentals(self):
         overdue = self.library.get_overdue_rentals()
-        self.assertEqual(len(overdue), 1)
-        self.assertTrue(overdue[0].is_overdue())
+        self.assertIn(self.rental1, overdue)
+        self.assertNotIn(self.rental2, overdue)
 
-    def test_get_all_members(self):
-        members = self.library.get_all_members()
-        self.assertEqual(len(members), 1)
-        self.assertEqual(members[0].name, "Adam Mickiewicz")
+    def test_get_all_members_with_overdue(self):
+        members_with_overdue = self.library.get_all_members_with_overdue()
+        self.assertIn(self.member1, members_with_overdue)
+        self.assertNotIn(self.member2, members_with_overdue)
+
+    def test_get_all_available_items(self):
+        available_items = self.library.get_all_available_items()
+        self.assertEqual(len(available_items), 1)
+        self.assertEqual(available_items[0], self.book2)
--- tests/unit/member_tests.py
+++ tests/unit/member_tests.py
@@ -1,35 +1,56 @@
 import unittest
+from datetime import datetime, timedelta
+from entities.item import Item
 from entities.member import Member
 from entities.rental import Rental
 
 
 class TestMember(unittest.TestCase):
     def setUp(self):
-        self.rental_not_overdue = Rental(is_overdue=False)
-        self.rental_overdue = Rental(is_overdue=True)
+        self.item1 = Item(id="1", title="Wiedźmin", year=1990, creator="Sapkowski")
+        self.item2 = Item(id="2", title="Dziady", year=1832, creator="Mickiewicz")
 
         self.member = Member(
             member_id="001",
             name="Adam",
             last_name="Mickiewicz",
             email="adam@example.com",
-            rented_items=[self.rental_not_overdue]
+            rented_items=[]
+        )
+
+        # Przeterminowane wypożyczenie (ręcznie dodane do listy)
+        self.overdue_rental = Rental(
+            item=self.item2,
+            member=self.member,
+            rent_date=datetime.now() - timedelta(days=40),
+            return_date=datetime.now() - timedelta(days=10),
+            returned=False
         )
 
     def test_borrow_item(self):
-        self.member.borrow_item(self.rental_overdue)
-        self.assertIn(self.rental_overdue, self.member.rented_items)
-        self.assertEqual(len(self.member.rented_items), 2)
+        self.member.borrow_item(self.item1)
+        self.assertEqual(len(self.member.rented_items), 1)
+        rental = self.member.rented_items[0]
+        self.assertEqual(rental.item.title, "Wiedźmin")
+        self.assertEqual(rental.member.name, "Adam")
+
+    def test_borrow_unavailable_item(self):
+        self.item1.available = False
+        self.member.borrow_item(self.item1)
+        self.assertEqual(len(self.member.rented_items), 0)
 
     def test_return_item(self):
-        self.member.borrow_item(self.rental_overdue)
-        self.member.return_item(self.rental_overdue)
-        self.assertNotIn(self.rental_overdue, self.member.rented_items)
-        self.assertEqual(len(self.member.rented_items), 1)
+        self.member.borrow_item(self.item1)
+        rental = self.member.rented_items[0]
+
+        self.member.return_item(rental)
+
+        self.assertTrue(rental.returned)
+        self.assertTrue(self.item1.available)
+        self.assertEqual(len(self.member.rented_items), 0)
 
     def test_get_overdue_rentals(self):
-        self.member.borrow_item(self.rental_overdue)
+        self.member.rented_items.append(self.overdue_rental)
         overdue = self.member.get_overdue_rentals()
-        self.assertIn(self.rental_overdue, overdue)
-        self.assertNotIn(self.rental_not_overdue, overdue)
         self.assertEqual(len(overdue), 1)
+        self.assertTrue(overdue[0].is_overdue())
